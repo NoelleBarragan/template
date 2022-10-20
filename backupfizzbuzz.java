294a155f4382f72bce58c969db5b8c93a3961cf2
Noelle Barragan <noelle.barragan@mrjaffesclass.com>
2022-10-20 14:00:40 -0700

    fizzbuzz complete

--- backupfizzbuzz.java
+++ backupfizzbuzz.java
@@ -1,30 +1,29 @@
 
 /**
- * Write a description of class RenameThisClass here.
+ * Write a description of class backupfizzbuzz here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class RenameThisClass
+public class backupfizzbuzz
 {
     public static void main(String[] args) {
         int i = 0;
-        System.out.println("----------------------------------------------------------");
         do {
             int a3 = i/3;
             int a5 = i/5;
             double b3 = i/3.0;
             double b5 = i/5.0;
             if (a3 == b3 || a5 == b5) {
-                if (a3 == b3 && a3 > 0) {
+                if (a3 == b3) {
                     System.out.println("Fizz");
                 }
-                if (a5 == b5 && a5 > 0) {
+                if (a5 == b5) {
                     System.out.println("Buzz");
                 }
-                if (a5 == b5 && a3 == b3 && a3 > 0) {
+                if (a5 == b5 && a3 == b3) {
                     System.out.println("FizzBuzz");
-                } 
+                } else {System.out.println(i);}
             } 
             else {System.out.println(i);}
             i +=1;
